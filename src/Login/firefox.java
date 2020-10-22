7c478e554c7d586c4086eca25ce9ebb08b490687
unknown <gulama@lembdatest.com>
2020-10-22 11:48:01 +0530

    first commit

---
+++ src/Login/firefox.java
@@ -0,0 +1,23 @@
+package Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class firefox {
+
+	public static void main(String [] args)
+	{
+		System.setProperty("webdriver.gecko.driver", "/D:/geckodriver.exe");
+		
+		FirefoxDriver driver = new FirefoxDriver();
+		
+		driver.get("https://accounts.lambdatest.com/login");
+	    driver.findElement(By.name("email")).sendKeys("gulama@lambdatest.com");
+	    driver.findElement(By.name("password")).sendKeys("lambdatest1");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+	    driver.manage().window().maximize();
+	    driver.quit();
+		
+	}
+}
